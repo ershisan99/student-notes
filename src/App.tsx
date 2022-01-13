abdbef4028739a6bc0c11a9f9a5d788eb040bd53
Andres <zadorozhnyi99.dev@gmail.com>
2022-01-13 21:26:38 +0100

    added basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Monday from "./Pages/Monday/Monday";
 import Tuesday from "./Pages/Tuesday/Tuesday";
 import StartPage from "./Pages/Start Page/StartPage";
-import {CssBaseline} from "@mui/material";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   return (
-      <>
-          <CssBaseline enableColorScheme/>
-        <BrowserRouter>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<StartPage />} />
-              <Route path="/monday" element={<Monday />} />
-              <Route path="/tuesday" element={<Tuesday />} />
-            </Routes>
-          </Layout>
-        </BrowserRouter>
-      </>
-  )}
+    <>
+      <CssBaseline enableColorScheme />
+      <BrowserRouter basename={"/student-notes"}>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<StartPage />} />
+            <Route path="/monday" element={<Monday />} />
+            <Route path="/tuesday" element={<Tuesday />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </>
+  );
+}
 
 export default App;
