cf962184df1f83a0baf917f96870e6eb44db1717
Andres <zadorozhnyi99.dev@gmail.com>
2022-01-13 21:29:17 +0100

    layout changes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,27 +2,26 @@ import React from "react";
 import { LeftDrawer } from "./LeftDrawer";
 
 const Layout: React.FC = ({ children }) => {
-    let contents = [
-        {
-            text: "Старт",
-            path: "/",
-        },
-        {
-            text: "Понедельник",
-            path: "/monday",
-        },
-        {
-            text: "Вторник",
-            path: "/tuesday",
-        },
-
-    ];
-    return (
-        <div>
-            <LeftDrawer contents={contents} />
-            {children}
-        </div>
-    );
+  let contents = [
+    {
+      text: "Старт",
+      path: "student-notes/",
+    },
+    {
+      text: "Понедельник",
+      path: "student-notes/monday",
+    },
+    {
+      text: "Вторник",
+      path: "student-notes/tuesday",
+    },
+  ];
+  return (
+    <div>
+      <LeftDrawer contents={contents} />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
